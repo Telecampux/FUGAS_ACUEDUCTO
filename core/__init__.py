39180aa1102aa3fbc56ec71ad9315bc16a75976c
Telecampux <telecampux@gmail.com>
2026-04-11 13:17:03 -0500

    limpeiza total

--- core/__init__.py
+++
@@ -1,5 +0,0 @@
-# Este archivo convierte la carpeta 'core' en un paquete de Python.
-# Al exponer las funciones aquí, las importaciones en app.py serán más limpias.
-
-from .hydraulics import haversine, perdida_hazen_williams
-from .config import territorios, PROGRAMA_NOMBRE, AUTOR, EMPRESA_DEFAULT
